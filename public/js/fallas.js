055ec25d3e71de33469d5ee185055e771c391958
Gaston Tomas <gaston.tomas.huete.1997@gmail.com>
2020-01-07 20:35:38 +0100

    selector y hasta casi terminado

--- public/js/fallas.js
+++ public/js/fallas.js
@@ -6,10 +6,17 @@ let jsonFile;
 //value de la seccion
 let seccionFiltro = "";
 
+//desde y hasta para el filtro
+
+let desde = "";
+let hasta = "";
+
+
 function init() {
 
     document.getElementsByName("seleccionTamano").forEach(element => element.addEventListener("change", promesaCreadoraDelTodo));
-    document.getElementById("selectSeccionFallas").addEventListener("change", cambiarFallas);
+    document.getElementById("selectSeccionFallas").addEventListener("change", promesaCreadoraDelTodo);
+    document.getElementById("desde").addEventListener("input", promesaCreadoraDelTodo);
     descargarJson();
 
 }
@@ -29,21 +36,50 @@ function descargarJson() {
     }).then(promesaCreadoraDelTodo);
 }
 
-function cambiarFallas() {
+let sec = 0;
 
-    seccionFiltro = this.value;
+
+//es la funcion que lanza despues de descargar todo el json
+function promesaCreadoraDelTodo() {
+
+    //detecta que es todas
+    if (document.getElementById("selectSeccionFallas").selectedIndex == -1) {
+        sec = 0;
+    } else {
+        sec = document.getElementById("selectSeccionFallas").selectedIndex;
+
+    }
+
+    console.log(document.getElementById("selectSeccionFallas").selectedIndex);
+
+    //vaciamos primero el div para mejor seleccion
     document.getElementById("listaFallas").innerHTML = "";
+    document.getElementById("selectSeccionFallas").innerHTML = "";
 
-    let filtro = jsonFile.features.filter(busquedaJson);
+    cargarSecciones();
 
-    //console.log("filtro realizado");
+    document.getElementById("selectSeccionFallas").selectedIndex = sec;
 
-    //esto es la creacion del "frame" donde se almacena la falla
-    filtro.forEach(iteracion => {
+    seccionFiltro = document.getElementById("selectSeccionFallas");
+
+    let filtroSecciones = jsonFile.features.filter(busquedaSeccion);
+    //console.log(filtroSecciones);
+    let filtroAnyo = filtroSecciones.filter(busquedaAnyo);
+    //console.log(filtroAnyo);
+    //console.log(document.getElementById("desde").value);
+    //console.log(document.getElementById("hasta").value);
+
+    filtroSecciones.forEach(iteracion => {
+
+        //------------------------------//
+        //aqui se crea la falla como tal//
+        //------------------------------//
         let falla = document.createElement("div");
         falla.classList.add("falla");
-        //let parrafo = document.createElement("p");
-        //let imagenP = document.createElement("img");
+        //falla.addEventListener("mouseover", mostrarInfoFalla);
+        //falla.addEventListener("mouseout", borrarClase);
+        let parrafo = document.createElement("p");
+        let imagenP = document.createElement("img");
         let imagenI = document.createElement("img");
         //let boton = document.createElement("button");
         //boton.innerHTML = "Ubicacion";
@@ -51,49 +87,6 @@ function cambiarFallas() {
         imagenI.src = iteracion.properties.boceto_i;
         imagenP.src = iteracion.properties.boceto;
         parrafo.innerHTML = iteracion.properties.nombre;
-
-        //creo el array de secciones con el set, que hace que no se repita lo que le pasen
-
-        //aqui añado todo lo creado iterativamente
-
-        if (document.getElementsByName("seleccionTamano")[1].checked) {
-            falla.appendChild(imagenI);
-        } else if (document.getElementsByName("seleccionTamano")[0].checked) {
-            falla.appendChild(imagenP);
-        }
-
-        falla.appendChild(parrafo);
-        //falla.appendChild(boton);
-        document.getElementById("listaFallas").appendChild(falla);
-    });
-
-}
-
-//es la funcion que lanza despues de descargar todo el json (creo que esta mal XD)
-function promesaCreadoraDelTodo() {
-
-    document.getElementById("listaFallas").innerHTML = "";
-    document.getElementById("selectSeccionFallas").innerHTML = "";
-
-    cargarSecciones();
-
-    jsonFile.features.forEach(iteracion => {
-        let falla = document.createElement("div");
-        falla.classList.add("falla");
-        falla.addEventListener("mouseover", mostrarInfoFalla);
-        let parrafo = document.createElement("p");
-        let imagenP = document.createElement("img");
-        let imagenI = document.createElement("img");
-        //let boton = document.createElement("button");
-        //boton.innerHTML = "Ubicacion";
-        //boton.addEventListener("click", llamadaUbicacion);
-        //imagenI.src = iteracion.properties.boceto_i;
-        //imagenP.src = iteracion.properties.boceto;
-        parrafo.innerHTML = iteracion.properties.nombre;
-
-        //creo el array de secciones con el set, que hace que no se repita lo que le pasen
-
-        //aqui añado todo lo creado iterativamente
 
         if (document.getElementsByName("seleccionTamano")[1].checked) {
             falla.appendChild(imagenI);
@@ -110,15 +103,44 @@ function promesaCreadoraDelTodo() {
 
 }
 //esta funcion devuelve true or false si se cumple la condicion
-function busquedaJson(iteracion) {
+//----------------------AQUI ESTAN LOS FILTROS----------------------------
+function busquedaSeccion(iteracion) {
 
-    if (document.getElementsByName("seleccionTamano")[0].checked) { // principal
-        return iteracion.properties.seccion.startsWith(seccionFiltro);
-    } else { // infantil
-        return iteracion.properties.seccion_i.startsWith(seccionFiltro);
+    console.log(seccionFiltro.value);
+
+    if (seccionFiltro.value == "Todas") {
+        return true;
+    } else {
+
+        if (document.getElementsByName("seleccionTamano")[0].checked) { // principal
+            return iteracion.properties.seccion.startsWith(seccionFiltro);
+        } else { // infantil
+            return iteracion.properties.seccion_i.startsWith(seccionFiltro);
+        }
     }
-
 }
+
+function busquedaAnyo(iteracion) {
+
+    let valorDesde = document.getElementById("desde").value;
+    let valorHasta = document.getElementById("hasta").value;
+
+    console.log(iteracion.properties.anyo_fundacion_i.startsWith());
+
+    if (document.getElementById("desde").value == '') {
+        return true
+    } else {
+
+        if (document.getElementsByName("seleccionTamano")[0].checked) { // principal
+            return iteracion.properties.anyo_fundacion.startsWith(valorDesde);
+        } else { // infantil
+            return iteracion.properties.anyo_fundacion_i.startsWith();
+        }
+    }
+}
+
+//------------------------------------------------------------------------
+
 
 //esta funcion carga las secciones, distinguiendo si es 
 function cargarSecciones() {
@@ -151,6 +173,13 @@ function cargarSecciones() {
 
 function mostrarInfoFalla(e) {
 
+    this.classList.add("hacerGrande");
     console.log(this);
+
+}
+
+function borrarClase(e) {
+
+    this.classList.remove("hacerGrande");
 
 }
